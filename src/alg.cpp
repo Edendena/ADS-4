165d4df4d76a444e002e87ebf334dae334e587de
Edendena <naras15191@gmail.com>
2025-04-06 23:16:50 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,8 @@
-#include <algorithm>
-#include <unordered_map>
-
+// Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
     int count = 0;
-    for (int i = 0; i < len; ++i) {
-        for (int j = i + 1; j < len; ++j) {
+    for (int i = 0; i < len - 1; i++) {
+        for (int j = i + 1; j < len; j++) {
             if (arr[i] + arr[j] == value) {
                 count++;
             }
@@ -15,51 +13,55 @@ int countPairs1(int *arr, int len, int value) {
 
 int countPairs2(int *arr, int len, int value) {
     int count = 0;
-    int left = 0;
-    int right = len - 1;
-
-    while (left < right) {
-        int sum = arr[left] + arr[right];
-        if (sum == value) {
-            int leftCount = 1, rightCount = 1;
-            while (left + 1 < right && arr[left] == arr[left + 1]) {
-                left++;
-                leftCount++;
-            }
-            while (right - 1 > left && arr[right] == arr[right - 1]) {
-                right--;
-                rightCount++;
-            }
-            count += leftCount * rightCount;
-            left++;
-            right--;
-        } else if (sum < value) {
-            left++;
-        } else {
-            right--;
-        }
+    int end = len - 1;
+    while (end > 0) {
+      if (arr[end] > value) {
+        end--;
+    } else {
+        break;
     }
+  }
+    for (int i = 0; i < len; i++) {
+      for (int j = end; j > i; j--) {
+        if (arr[i] + arr[j] == value)
+          count++;
+    }
+  }
     return count;
+}
+
+int Step(int *arr, int left, int right, int value) {
+    int first = -1;
+    int mleft = left, mright = right;
+    while (mleft <= mright) {
+      int mid = mleft + (mright - mleft) / 2;
+      if (arr[mid] >= value) {
+        mright = mid - 1;
+        if (arr[mid] == value) first = mid;
+    } else {
+        mleft = mid + 1;
+    }
+  }
+    if (first == -1) return 0;
+    int last = first;
+    mleft = first;
+    mright = right;
+    while (mleft <= mright) {
+      int mid = mleft + (mright - mleft) / 2;
+      if (arr[mid] <= value) {
+        mleft = mid + 1;
+        if (arr[mid] == value) last = mid;
+    } else {
+        mright = mid - 1;
+    }
+  }
+    return last - first + 1;
 }
 
 int countPairs3(int *arr, int len, int value) {
     int count = 0;
-    std::unordered_map<int, int> freq;
-
     for (int i = 0; i < len; ++i) {
-        freq[arr[i]]++;
+        count += Step(arr, i + 1, len - 1, value - arr[i]);
     }
-
-    for (int i = 0; i < len; ++i) {
-        int complement = value - arr[i];
-        if (freq.find(complement) != freq.end()) {
-            count += freq[complement];
-            if (complement == arr[i]) {
-                count--;
-            }
-        }
-    }
-    return count / 2;
+    return count;
 }
-
-
